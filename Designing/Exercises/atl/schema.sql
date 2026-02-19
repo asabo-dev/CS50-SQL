8c139b6e16cd2ebc430dab27241bdbfb75b0f9a6
Efiom Ndaeyo <rylshadow@yahoo.com>
2026-02-19 21:08:58 +0800

    Update Designing DB Exercise 1(atl)

--- Designing/Exercises/atl/schema.sql
+++ Designing/Exercises/atl/schema.sql
@@ -1,3 +1,4 @@
+-- First name, last name, and age. That’s all we need to know
 CREATE TABLE "Passengers" (
     "id" INTEGER,
     "first_name" TEXT NOT NULL,
@@ -6,6 +7,8 @@ CREATE TABLE "Passengers" (
     PRIMARY KEY("id")
 );
 
+-- The exact date and time at which our passenger checked in.
+-- The flight they are checking in for.
 CREATE TABLE "Check_Ins" (
     "passenger_id" INTEGER,
     "check_in_time" TEXT NOT NULL, -- store as 'YYYY-MM-DD HH:MM:SS'
@@ -14,17 +17,27 @@ CREATE TABLE "Check_Ins" (
     FOREIGN KEY("flight_id") REFERENCES "Flights"("flight_number")
 );
 
+-- The name of the airline
+-- The “concourse”, the section of our airport where the airline operates. 
 CREATE TABLE "Airlines" (
     "name" TEXT,
     PRIMARY KEY("name")
 );
 
+-- We have 7 concourses: A, B, C, D, E, F, and T.
 CREATE TABLE "Airline_Concourses" (
     "airline_name" TEXT,
     "concourse" TEXT,
     PRIMARY KEY("airline_name", "concourse"),
     FOREIGN KEY("airline_name") REFERENCES "Airlines"("name")
 );
+
+-- The flight number. For example, “900”.
+-- The airline operating the flight. 
+-- The code of the airport they’re departing from. For example, “ATL” or “BOS”.
+-- The code of the airport they’re heading to.
+-- The expected departure date and time (to the minute, of course!).
+-- The expected arrival date and time, to the very same accuracy.
 
 CREATE TABLE "Flights" (
     "flight_number" TEXT,
